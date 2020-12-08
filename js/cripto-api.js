6cc830ab007d59942e4c74630d68d7bd6487c809
werbethe <werbethe@medware.com.br>
2020-12-08 00:13:40 -0300

    adicionado exibir moedas

--- js/cripto-api.js
+++ js/cripto-api.js
@@ -1,9 +1,25 @@
 const apiKey = "";
 
+
+
 const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=${apiKey}`;
 fetch(url).then(resposta => {
     if (!resposta.ok) throw new Error(`Ocorreu um erro na requisição, status do erro ${resposta.status}`);
     return resposta.json();
-}).then(resposta => {
-    console.log(resposta);
-}).catch(erro => console.erro(erro));
+}).then(api => {
+    let texto = "";
+    for (let i = 0; i < 100; i++) {  //100 primeiras moedas
+        const moeda = api.data[i];
+        texto += `
+            <div class="media">
+                <img src="coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+                <div class="media-body">
+                    <h5 class="mt-2">${moeda.name}</h5>
+                    <p>${moeda.symbol}</p>
+                </div>
+            </div>
+            `;
+
+        document.getElementById("moedas").innerHTML = texto;
+    }
+}).catch(erro => console.erro(erro.message));
