72ce8b05d4233001c0307d39931dfb459cdd28a5
werbethe <werbethe@medware.com.br>
2020-12-07 23:43:24 -0300

    adicionado busca na api

--- js/cripto-api.js
+++ js/cripto-api.js
@@ -1,1 +1,9 @@
 const apiKey = "";
+
+const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=${apiKey}`;
+fetch(url).then(resposta => {
+    if (!resposta.ok) throw new Error(`Ocorreu um erro na requisição, status do erro ${resposta.status}`);
+    return resposta.json();
+}).then(resposta => {
+    console.log(resposta);
+}).catch(erro => console.erro(erro));
